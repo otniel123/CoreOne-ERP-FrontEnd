e3ea8f8336845e7f2f88bbe01c87a4c23b9ed98f
Otniel Marques <Otniel.Marques@philips.com>
2026-05-01 04:14:00 -0300

    Tela de login finalizada

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { Users } from './users/users';
 import { Login } from "./login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [Login, Users],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  cadastrandoUsuario: boolean = false;
   protected readonly title = signal('CoreOne-Frontend');
+
+  setCadastrarUsuarioTrue(){
+    this.cadastrandoUsuario = true;
+  }
 }
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,12 @@ import { Component } from '@angular/core';
 export class Login {
   email ?: string;
   password ?: string;
+  errorMessage : string = "";
+  loggedIn : boolean = false;
+  loggedUserEmail ?: string;
+  recentActions = ['Login realizado', 'Perfil acessado', 'Dashboard aberto'];
+
+  @Output() cadastrarUsuario = new EventEmitter()
 
   onInputEmail(event: Event){
     const input = event.target as HTMLInputElement;
@@ -20,8 +27,52 @@ export class Login {
     this.password = input.value;
   }
 
-  onLogin(event: Event){
+  onLogin(){
+    return;
+  }
+
+  onClickLoginButton(event: Event){
     event.preventDefault();
-    alert(this.email + " " +this.password);
+    if(!this.email?.trim() || !this.password?.trim()){
+      this.errorMessage = "Preencha todos os campos!";
+      return;
+    }
+    if(this.email?.includes(" ")){
+      this.errorMessage = "Email não pode conter espaços em branco";
+      return;
+    }
+    if(!this.email?.includes("@")){
+      this.errorMessage = "Inclua um '@' no endereço de email";
+      return;
+    }
+    let afterAtSign = this.email.indexOf("@") + 1;
+    if(this.email.charAt(afterAtSign) == ""){
+      this.errorMessage = "Insira uma parte depois de '@'"
+      return;
+    }
+    let domain = this.email.slice(afterAtSign);
+    if(!domain.includes(".com")){
+      this.errorMessage = "Insira um dominio válido. Ex: exemplo@gmail.com"
+      return;
+    }
+    this.loggedIn = true;
+    this.loggedUserEmail = this.email;
+    this.errorMessage = "";
+    return;
+  }
+
+  clickBotaoCadastrarUsuario(){
+    this.cadastrarUsuario.emit(true);
+  }
+
+  onLogout(){
+    this.email = "";
+    this.password = "";
+    this.loggedIn = false;
+    this.loggedUserEmail = ""
+  }
+
+  get getLoggedIn(){
+    return this.loggedIn;
   }
 }
