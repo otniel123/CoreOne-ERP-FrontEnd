2949402a9848ea05c606fb501db889ea966b4451
Otniel Marques <Otniel.Marques@philips.com>
2026-05-04 20:34:54 -0300

    HTML e CSS das telas pronto

---
+++ src/app/register/register.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  onSubmit(): void {
+    // TODO: implementar cadastro
+  }
+}
